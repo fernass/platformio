b39fe162120384a8ea343052f33dfb8f7c26585b
Fernass Daoud <fernass.daoud@web.de>
2021-11-30 19:08:16 +0100

    initial src for pwm example: control led brightness with pot

---
+++ led_pot/src/main.c
@@ -0,0 +1,14 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "driver/ledc.h"
+
+
+static void init_hw(void);
+
+void app_main() {}
+
+
+static void init_hw(void){
+    
+}
