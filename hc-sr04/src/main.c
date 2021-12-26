2005202d5154cdea420796b6b5242f4a897ecc13
Fernass Daoud <fernass.daoud@web.de>
2021-12-26 21:56:05 +0100

    version 1 finished and tested

--- hc-sr04/src/main.c
+++ hc-sr04/src/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include "freertos/semphr.h"
-#include "esp_err.h"
-#include "esp_log.h"
 #include "driver/rmt.h"
-#include "driver/periph_ctrl.h"
-#include "soc/rmt_reg.h"
-#include <sys/time.h>
-#include "driver/gpio.h"
+
 
 #define RMT_TX_CHANNEL 1            /* RMT channel for transmitter */
 #define RMT_TX_GPIO_NUM PIN_TRIGGER /* GPIO number for transmitter signal */
