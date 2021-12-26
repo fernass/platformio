0c9a832149e70a1a3c55de83e2529441cc6c4fbd
Fernass Daoud <fernass.daoud@web.de>
2021-12-26 21:56:40 +0100

    servo: initial commit

---
+++ servo/src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/ledc.h"
+
+
+/*
+using ledc driver in 3 steps:
+1) timer config
+2) channel config
+3) change pwm signal
+*/
+
+void init_hw(){
+    ledc_timer_config_t timer_config = {
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .duty_resolution = 
+    };
+}
+
+void app_main() {}
